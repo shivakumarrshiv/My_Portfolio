a3630afead66c806d766c84f6f031178f69c2705
Shivakumar R <143021952+shivakumarrshiv@users.noreply.github.com>
2025-05-23 08:35:56 +0530

    Add files via upload

---
+++ src/components/Projects/index.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import { useState } from 'react'
+import { Container, Wrapper, Title, Desc, CardContainer, ToggleButtonGroup, ToggleButton, Divider } from './ProjectsStyle'
+import ProjectCard from '../Cards/ProjectCards'
+import { projects } from '../../data/constants'
+
+
+const Projects = ({openModal,setOpenModal}) => {
+  const [toggle, setToggle] = useState('all');
+  return (
+    <Container id="projects">
+      <Wrapper>
+        <Title>Projects</Title>
+        <Desc>
+          I have worked on a wide range of projects, from Python full-stack and Java-based applications to machine learning and deep learning projects, utilizing React for designing the front end in all of them.
+        </Desc>
+        <ToggleButtonGroup >
+          {toggle === 'all' ?
+            <ToggleButton active value="all" onClick={() => setToggle('all')}>All</ToggleButton>
+            :
+            <ToggleButton value="all" onClick={() => setToggle('all')}>All</ToggleButton>
+          }
+          <Divider />
+          {toggle === 'web app' ?
+            <ToggleButton active value="web app" onClick={() => setToggle('web app')}>WEB APP'S</ToggleButton>
+            :
+            <ToggleButton value="web app" onClick={() => setToggle('web app')}>WEB APP'S</ToggleButton>
+          }
+          <Divider />
+          {toggle === 'python projects' ?
+            <ToggleButton active value="python projects" onClick={() => setToggle('python projects')}>PYTHON PROJECT'S</ToggleButton>
+            :
+            <ToggleButton value="python projects" onClick={() => setToggle('python projects')}>PYTHON PROJECT'S</ToggleButton>
+          }
+          <Divider />
+          {toggle === 'machine learning' ?
+            <ToggleButton active value="machine learning" onClick={() => setToggle('machine learning')}>MACHINE LEARNING</ToggleButton>
+            :
+            <ToggleButton value="machine learning" onClick={() => setToggle('machine learning')}>MACHINE LEARNING</ToggleButton>
+          }
+        </ToggleButtonGroup>
+        <CardContainer>
+          {toggle === 'all' && projects
+            .map((project) => (
+              <ProjectCard project={project} openModal={openModal} setOpenModal={setOpenModal}/>
+            ))}
+          {projects
+            .filter((item) => item.category == toggle)
+            .map((project) => (
+              <ProjectCard project={project} openModal={openModal} setOpenModal={setOpenModal}/>
+            ))}
+        </CardContainer>
+      </Wrapper>
+    </Container>
+  )
+}
+
+export default Projects
---
+++ src/data/constants.js
@@ -0,0 +1,336 @@
+export const Bio = {
+  name: "Shivakumar R",
+  roles: [
+    "Python Developer",
+    "Java Full Stack Developer",
+    "Data Scientist",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/shivakumarrshiv",
+  resume:"https://drive.google.com/file/d/1TW4Rc_W54Lpd8QAiISalwV49ecYmn7vk/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/shivakumar9148",
+  twitter: "https://twitter.com/RishavChanda",
+  insta: "https://twitter.com/shivakumarrshiv",
+  facebook: "https://www.facebook.com/profile.php?id=100030939954174&mibextid=rS40aB7S9Ucbxw6v",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Python",
+        image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "Java",
+        image: "https://www.vectorlogo.zone/logos/java/java-icon.svg",
+      },
+      {
+        name: "FastAPI",
+        image: "https://github.com/fastapi/fastapi/blob/master/docs/en/docs/img/logo-margin/logo-teal.svg",
+      },
+      {
+        name: "Spring Boot",
+        image:
+          "https://www.vectorlogo.zone/logos/springio/springio-icon.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Postgresql",
+        image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "Flask",
+        image: "https://www.vectorlogo.zone/logos/palletsprojects_flask/palletsprojects_flask-icon.svg",
+      },
+    ],
+  },
+  {
+    title: "Data Science",
+    skills: [
+      {
+        name: "Artificial Intelligence",
+        image:
+          "https://img.icons8.com/external-ddara-fill-ddara/64/external-data-science-managements-ddara-fill-ddara.png",
+      },
+      {
+        name: "Machine Learning",
+        image:
+          "https://img.icons8.com/ios/50/machine-learning.png",
+      },
+      {
+        name: "Deep Learning",
+        image:
+          "https://img.icons8.com/external-becris-lineal-becris/64/external-deep-learning-artificial-intelligence-becris-lineal-becris.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://camo.githubusercontent.com/93b32389bf746009ca2370de7fe06c3b5146f4c99d99df65994f9ced0ba41685/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f676574706f73746d616e2f676574706f73746d616e2d69636f6e2e737667",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    role: "Machine Learning Intern",
+    company: "Center of Excellence RVCE.",
+    date: "July 2023 - Dec 2023",
+    desc: "Working Automated Telecom Complaint Management System",
+    skills: [
+      "Docker",
+      "Machine Learning",
+      "Python",
+      "EC2",
+      "React.JS",
+      "Postgresql",
+      "LLMs",
+      "FastAPI",
+      "Tensorflow"
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    school: "R.V. College of Engineering, Bengaluru",
+    date: "Dec 2021 - Aug 2024",
+    grade: "7.1 CGPA",
+    desc: "I have completed my Bachelor's degree in Electronics and Telecommunication Engineering at R.V. College of Engineering (RVCE), Bengaluru, with a CGPA of 7.1. My course duration was from December 1, 2021 to August 31, 2024. During my studies, I completed courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+    degree: "Bachelor of Engineering - B.E, Electronics and Telecommunication Engineering"
+  },
+  {
+    id: 1,
+    school: "M.E.I Polytechnics, Bengaluru",
+    date: "Apr 2017 - Apr 2020",
+    grade: "76.2%",
+    desc: "I completed my Diploma in Electronics and Communication Engineering at MEI Polytechnic, Bengaluru. During this program, I built a strong foundation in electronic circuits, communication systems, and digital technologies, which prepared me for advanced engineering studies.",
+    degree: "Diploma, Electronics and Communication"
+  },
+  {
+    id: 2,
+    school: "C.K.P.H.S, Bengaluru",
+    date: "Apr 2016 - Apr 2017",
+    grade: "90%",
+    desc: "I completed my Class 10 education at CKPHS, Bengaluru.",
+    degree: "10th, Science with Computer"
+  }
+];
+
+
+export const projects = [
+  {
+    id: 9,
+    title: "Trackify",
+    date: "Jun 2023 - Jul 2023",
+    description:
+      "Trackify is a web application designed to streamline task management and enhance productivity in the workplace. It provides a user-friendly interface for employers to keep track of their employees' daily work activities and empowers employees to log their tasks efficiently.\nAdmin Credentials: # Email: testadmin@gmail.com #Password- 123@testadmin, Employee Credentials: #Email: testemployee@gmail.com #Password- 123@Testemployee",
+    image:
+      "https://user-images.githubusercontent.com/64485885/255202416-e1f89b04-2788-45b0-abc2-9dec616669e2.png",
+    tags: [
+      "Docker",
+      "AWS",
+      "DuckDNS",
+      "Eslint",
+      "Husky",
+      "CI/CD",
+      "React Js",
+      "MongoDb",
+      "Node Js",
+      "Express Js",
+      "Redux",
+    ],
+    category: "web app",
+    github: "https://github.com/shivakumarshiv/Trackify",
+    webapp: "https://trackify.duckdns.org",
+  },
+  {
+    id: 0,
+    title: "Podstream",
+    date: "Apr 2023 - May 2023",
+    description:
+      "Developed a full-stack web application that allows users to search for, play, and pause their favorite podcasts on demand and create podcasts. Implemented user authentication using Google Auth and Jwt Auth, made responsive user interface with React JS that provides users with a seamless experience across all devices. Practiced agile methodologies to optimize team efficiency and communication.",
+    image:
+      "https://user-images.githubusercontent.com/64485885/234602896-a1bd8bcc-b72b-4821-83d6-8ad885bf435e.png",
+    tags: ["React Js", "MongoDb", "Node Js", "Express Js", "Redux"],
+    category: "web app",
+    github: "https://github.com/shivakumarshiv/Podstream",
+    webapp: "https://podstream.netlify.app/",
+  },
+  {
+    id: 1,
+    title: "Whatsapp Clone",
+    date: "Jul 2021",
+    description:
+      "A WhatsApp clone made with React JS, Firebase, and Material UI. It has Phone Authentication, Real-time Database. It has a chat room where users can chat with each other. It has a sidebar where users can see all the chat rooms and can create a new chat room. It has a login page where users can log in with their Google account.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/whatsapp-clone-rishav.appspot.com/o/Screenshot%20(151).png?alt=media&token=48391593-1ef0-4a8c-a92a-eb82bdf38e89",
+    tags: ["React Js", "Firebase", "Firestore", "Node JS"],
+    category: "web app",
+    github: "https://github.com/shivakumarshiv/Whatsapp-Clone-React-Js",
+    webapp: "https://whatsapp-clone-rishav.web.app",
+  },
+  {
+    id: 4,
+    title: "Todo Web App",
+    date: "Jun 2021",
+    description:
+      "A Todo Web App made with React JS, Redux, and Material UI. It has a login page where users can log in with their Google account. It has a sidebar where users can see all the tasks and can create a new task. It has a calendar where users can see all the tasks on a particular date. It has a search bar where users can search for a particular task.",
+    image:
+      "https://github.com/shivakumarrshiv/First-Full-Stack-Project/blob/main/Todo.jpg?raw=true",
+    tags: ["React Js", "Local Storage", "AWS Auth", "Node JS"],
+    category: "web app",
+    github: "https://github.com/shivakumarshiv/Todo-Web-App",
+    webapp: "https://rishav-react-todo.netlify.app/",
+  },
+  {
+    id: 5,
+    title: "Landslide Prediction in Satellite Imagery",
+    date: "2024",
+    description:
+      "Developed a landslide prediction model using Python and deep learning techniques to analyze satellite images. Improved model performance by using data preprocessing techniques like resizing, normalization, and data augmentation to optimize satellite image analysis. Applied U-Net segmentation to highlight landslide-prone areas, improving the model’s ability to identify at-risk regions.",
+    image:
+      "https://github.com/shivakumarrshiv/First-Full-Stack-Project/blob/main/Landslide%20Images.jpeg?raw=true",
+    tags: ["Python", "Flask", "React", "Deep Learning", "U-Net", "Satellite Imagery"],
+    category: "python projects",
+    github:
+      "https://github.com/shivakumarrshiv/-Landslide-Prediction-in-Satellite-Imagery-Using-Deep-Learning",
+    webapp:
+      "https://github.com/shivakumarrshiv/-Landslide-Prediction-in-Satellite-Imagery-Using-Deep-Learning",
+  },
+  {
+    id: 6,
+    title: "Full-stack E-commerce Website",
+    date: "2024",
+    description:
+      "Developed a full-stack e-commerce platform using Python Flask and React.js, integrating a dynamic and responsive frontend with a powerful backend. Implemented secure login/registration features, wallet, cart, and seamless user interface with HTML, CSS, and React.js for smooth user experience. Managed data with SQLAlchemy, including password validation for enhanced security.",
+    image:
+      "https://github.com/shivakumarrshiv/First-Full-Stack-Project/blob/main/Full-%20Stack.png?raw=true",
+    tags: ["Python", "Flask", "React Js", "SQLAlchemy", "Git", "E-commerce"],
+    category: "python projects",
+    github:
+      "https://github.com/shivakumarrshiv/Full-Stack-E-Commerce-Platform-using-Python-Flask-framework",
+    webapp:
+      "https://github.com/shivakumarrshiv/Full-Stack-E-Commerce-Platform-using-Python-Flask-framework",
+  },
+  {
+    id: 7,
+    title: "Kidney-Disease-Classification-Deep-Learning-Project",
+    date: "2024",
+    description:
+      "Designed and implemented a deep learning model using CNNs to classify leaf diseases, ensuring high accuracy in disease prediction and classification. Applied data preparation strategies like image scaling, feature normalization, and synthetic data generation to improve model prediction accuracy. Developed a full-stack application using FastAPI for backend services and React.js for the frontend, deployed on AWS S3 for scalable and efficient file storage.",
+    image:
+      "https://github.com/shivakumarrshiv/First-Full-Stack-Project/blob/main/Kidney_Tumor%20Prdiction.jpg?raw=true",
+    tags: ["Python", "FastAPI", "Deep Learning", "React.js", "AWS"],
+    category: "python projects",
+    github: "https://github.com/shivakumarrshiv/Kidney-Disease-Classification-Deep-Learning-Project",
+    webapp: "",
+  },
+  {
+    id: 8,
+    title: "Face Recognition",
+    date: "Jan 2021",
+    description:
+      "A Face recognition python app made with OpenCV. It uses face_recognition library to detect faces. It uses the webcam to detect faces. It also has a search bar to search for a particular face.",
+    image:
+      "https://github.com/shivakumarrshiv/First-Full-Stack-Project/blob/main/Faces.jpeg?raw=true",
+    tags: ["Python", "Keras", "TensorFlow", "VGG16", "Pickle", "React"],
+    category: "machine learning",
+    github: "https://github.com/shivakumarshiv/Face-Recodnition-AI-with-Python",
+    webapp: "https://github.com/rishavchanda/Face-Recodnition-AI-with-Python",
+  },
+  {
+    id: 10,
+    title: "Brain Tumor Detection",
+    date: "Jan 2023 - Mar 2023",
+    description:
+      "Preprocessed and augmented the dataset to improve model accuracy, trained the model, created API using model and Python, and used React web app for the project's front end. Achievements: Achieved an accuracy of 99.2% to accurately detect brain tumors from medical images.",
+    image:
+      "https://github.com/rishavchanda/Brain-Tumor-Detection/raw/main/Readme_resource/Image2.png",
+    tags: ["Python", "Keras", "TensorFlow", "VGG16", "Pickle", "React"],
+    category: "machine learning",
+    github: "https://github.com/rishavchanda/Brain-Tumor-Detection",
+    webapp: "https://brain-tumor.netlify.app/",
+  },
+];
+
+
+export const TimeLineData = [
+  { year: 2021, text: "Started my journey in Programming" },
+  { year: 2022, text: "Developed DSA Skills" },
+  { year: 2023, text: "Worked as Machine Learning Intern" }
+];
